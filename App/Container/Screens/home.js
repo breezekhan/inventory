fcb22d0e87618031658ff2b0be0bb9aa69756cf8
breezekhan <sameersanukhan@gmail.com>
2024-09-19 04:39:55 +0530

    home screen

--- App/Container/Screens/home.js
+++ App/Container/Screens/home.js
@@ -1,12 +1,181 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, BackHandler, Image, Alert, Modal, FlatList, Linking, ImageBackground, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import styles from '../Styles/home_style'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import LoadingModal from '../../Utils/LoadingModal';
+import Images from '../../Utils/Images';
+import { getCompId, getUserId, getPackageId, getSocietyId, setSocietyId, getUserName } from '../../Utils/MasterFunctions';
+import { Colors } from '../../helpers';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import { useRoute } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 
-const home = () => {
-  return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
+
+import Toast from 'react-native-simple-toast';
+
+import moment from 'moment';
+import Fonts from '../../helpers/Fonts';
+
+import { orEmptyString, showToast } from '../../Utils/Utils';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Home = ({ navigation }) => {
+    const route = useRoute();
+    const [loading, setLoading] = useState(false);
+    const [limit, setLimit] = useState(10);
+    const [name, setName] = useState("")
+
+    const [data, setData] = useState([]);
+
+
+    const currentScreenName = route.name;
+    const handleBackPress = () => {
+        if (currentScreenName === "home") {
+            BackHandler.exitApp();
+            return true;
+        }
+        return false;
+    };
+
+    useFocusEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+        return () => BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    });
+
+    useEffect(() => {
+        getUserIDFun();
+    }, [])
+    const getUserIDFun = async () => {
+
+        let userId = await getUserId();
+        let myname = await getUserName();
+
+
+        setName(myname)
+
+
+    };
+
+
+
+
+
+    useEffect(() => {
+        GetData();
+    }, [limit]);
+
+    const GetData = async () => {
+        try {
+            console.log("chlaaa", limit)
+            setLoading(true);
+            const res = await fetch(`https://fakestoreapi.com/products?limit=${limit}`);
+            const result = await res.json();
+            if (result.length > 0) {
+
+                setData(result);
+            }
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+    };
+
+    const LoadMore = () => {
+        console.log("chlaaa")
+        if (!loading) {
+            setLimit((prevLimit) => prevLimit + 10);
+            GetData()
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <View style={styles.Container}>
+            <View style={styles.Header}>
+                <View style={styles.row_header}>
+                    <Image source={Images.LOGO}
+                        style={{ height: hp('8%'), width: wp('16%'), resizeMode: 'contain' }} />
+                    <View style={{ width: wp('60%') }} >
+                        <Text numberOfLines={2} style={styles.text_header}>Hello,  <Text style={styles.txt_name}>{name}</Text></Text>
+
+
+                    </View>
+                </View>
+
+            </View>
+
+
+
+
+
+
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                ListEmptyComponent={() => (
+                    <View>
+                        <Text>no data found</Text>
+                    </View>
+                )}
+                renderItem={({ item, index }) => (
+                    <View>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('product_detail', { data: item })}
+                            style={[styles.box, styles.shadowProp]}>
+                           
+                                <Image source={{ uri: item.image }} style={[styles.box_img]} />
+                               
+                          
+                            <View style={{flex:1,marginHorizontal:wp("4%"),justifyContent:'center'}}>
+                                <Text numberOfLines={2} style={styles.txt_pro}>{item.title}</Text>
+                                <Text numberOfLines={2} style={styles.txt_pro2}>{item.category}</Text>
+                               
+                               <View style={styles.row}>
+                               <View style={styles.row}>
+                                <Text numberOfLines={2} style={styles.txt_pro2}>{item.rating.rate}</Text>
+                                <AntDesign name={"star"} color={"gold"} size={wp("4%")} />
+                                </View>
+                                <View style={[styles.box_trans]}>
+                                    <Text style={[styles.txt_price, { marginLeft: wp('1%') }]}>${item.price}</Text>
+                                </View>
+                               
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                )}
+                onEndReached={() => LoadMore()}
+                onEndReachedThreshold={0.5}
+
+            />
+
+
+
+
+            <LoadingModal modalVisible={loading} />
+
+
+
+        </View>
+    )
 }
 
-export default home
+export default Home
--- App/Container/Styles/home_style.js
+++ App/Container/Styles/home_style.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create(
         },
         row: {
             alignItems: 'center',
-            // justifyContent: 'space-between',
+             justifyContent: 'space-between',
             flexDirection: 'row',
         },
         Text: {
@@ -55,14 +55,7 @@ const styles = StyleSheet.create(
             marginVertical: wp('0.5%')
 
         },
-        bell: {
-            backgroundColor: Colors.Primary,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: wp('100%'),
-            height: hp('6%'), width: wp('12%'),
-        },
-
+      
         //SWIPER
         dotStyle: {
             backgroundColor: 'rgba(0, 0, 0, 0.2)',
@@ -85,60 +78,7 @@ const styles = StyleSheet.create(
             marginBottom: 3,
         },
 
-        bannerImage: {
-            width: wp("95%"),
-            height: hp("15%"),
-            alignSelf: 'center',
-            resizeMode: 'contain'
-        },
-        //BOX
-        cat_box: {
-            borderRadius: 10,
-            backgroundColor: '#ffffff',
-            // height: hp('20%'),
-            width: wp('28%'),
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: wp('2%')
-        },
-        cat_box_Society: {
-            borderRadius: 10,
-            backgroundColor: '#ffffff',
-            // height: hp('20%'),
-            width: wp('45%'),
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: wp('10%')
-        },
-        cat_img: {
-            height: hp('8%'), width: wp('16%'), resizeMode: 'contain'
-        },
-        cat_box1: {
-            borderRadius: 10,
-            backgroundColor: '#ffffff',
-            // height: hp('20%'),
-            // width: wp('21%'),
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingVertical: wp('2%'),
-            paddingHorizontal: wp('6%')
-        },
-        cat_img1: {
-            height: hp('8%'), width: wp('16%'), resizeMode: 'contain'
-        },
-        cat_box2: {
-            borderRadius: wp('2%'),
-            backgroundColor: '#ffffff',
-            // height: hp('20%'),
-            width: wp('21%'),
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingVertical: wp('2%'),
-            //  paddingHorizontal:wp('4%')
-        },
-        cat_img2: {
-            height: hp('5%'), width: wp('10%'), resizeMode: 'contain'
-        },
+       
         shadowProp: {
             shadowOffset: { width: -2, height: 4 },
             shadowColor: 'black',
@@ -173,18 +113,7 @@ const styles = StyleSheet.create(
             marginVertical: hp('1%'),
         },
         //PRODUCT
-        box: {
-            borderRadius: 10,
-            backgroundColor: '#ffffff',
-            height: hp('12%'),
-          //  width: wp('40%'),
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            paddingHorizontal: wp('2%'),
-            marginHorizontal:wp('2%'),
-            marginVertical:hp('1%'),
-            flexDirection:'row'
-        },
+       
         
         txt:{
             fontSize:14,
@@ -193,37 +122,40 @@ const styles = StyleSheet.create(
             marginVertical:"1%"
            },
         box: {
-            borderRadius: 10,
+            borderRadius: wp("2%"),
             backgroundColor: '#ffffff',
-            height: hp('25%'),
-            width: wp('42.8%'),
-            alignItems: 'center',
-            justifyContent: 'center',
+           // height: hp('25%'),
+            flexDirection:'row',
+           // width: wp('42.8%'),
+           // alignItems: 'center',
+           justifyContent: 'space-between',
             paddingHorizontal: wp('2.5%'),
             marginHorizontal: wp('2%'),
             marginVertical: hp('2%'),
         },
         box_img: {
-            height: hp('25%'), width: wp('40%'), resizeMode: 'contain'
+            height: hp('16%'), width: wp('24%'),resizeMode:'contain'
         },
         box_trans: {
-            bottom: 0,
-            position: 'absolute',
-            marginBottom: hp('1%'),
+            // bottom: 0,
+            // position: 'absolute',
+            // marginBottom: hp('1%'),
             backgroundColor: 'rgba(255, 255, 255, 0.6)',
-            // backgroundColor: 'rgba(128, 128, 128, 0.4)',
+           // marginHorizontal:wp("1%"),
             borderWidth: wp('0.1'),
             borderColor: 'rgba(128, 128, 128, 0.4)',
             borderRadius: wp('2%'),
             padding: wp('1%'),
-            width: wp('40%')
+            width: wp('30%'),
+            alignItems:'center'
         },
         txt_price: {
             fontSize: wp('3.5%'),
             // color: '#21de8f',
             color: Colors.price,
             marginVertical: hp('0.2%'),
-            fontFamily: Fonts.Lato_Bold
+            fontWeight:'bold'
+            
         },
         offerTxt: {
             //flex: 0.70,
@@ -235,201 +167,30 @@ const styles = StyleSheet.create(
             // textDecorationLine: 'line-through'
         },
         txt_pro: {
-            fontSize: wp('3.2%'),
-            color: Colors.black,
-            // marginVertical: hp('0.2%'),
-          //  width:wp('35%'),
-            fontFamily: Fonts.Lato_Bold
+            fontSize: wp('3.5%'),
+            color: Colors.Primary,
+            fontWeight:'bold',
+            marginVertical: hp('0.5%'),
+           
         },
         txt_pro2: {
             fontSize: wp('3.5%'),
             color: Colors.black,
             marginVertical: hp('0.5%'),
-            fontFamily:Fonts.Lato_Reguler,
+            
         },
         row2:{
             flexDirection:'row',
             alignItems:'center',
             justifyContent:'space-between'
         },
-       //SPECIAL
-        Image_special: {
-            width: wp("95%"),
-            height: hp("20%"),
-            alignSelf: 'center',
-            resizeMode: 'contain'
-        },
-        text_key: {
-            fontSize: wp('3.5%'),
-            color: Colors.black,
-            fontWeight: 'bold',
-            //marginVertical: hp('0.5%'),
-            width: wp('30%'), textAlign: 'left'
-        },
-        text_das: {
-            fontSize: wp('3.5%'),
-            color: Colors.dark_grey,
-            fontWeight: 'bold',
-            // marginVertical: hp('0.5%'),
-            width: wp('5%'), textAlign: 'center'
-        },
-        text_value: {
-            fontSize: wp('3.5%'),
-            color: Colors.black,
-            // fontWeight: 'bold',
-            // marginVertical: hp('0.5%'),
-            textAlign: 'justify',
-            width: wp('50%')
-        },
-        modalbox: {
-            // alignItems: 'center',
-            // borderWidth: wp('0.1%'),
-            padding: wp('1%'),
-            borderRadius: wp('2%'),
-            marginVertical: hp('1%')
-        },
-        txt_modal: {
-            color: Colors.black,
-            fontSize: wp('4%'),
-          // padding:wp('1%')
-            // fontWeight: 'bold'
-        },
-        List: {
-            marginHorizontal: wp('4%'),
-            marginVertical: wp('1%'),
-            flex: 1
-        },
-        ////
-        container1: {
-            flex: 1,
-            //alignItems: 'center', 
-            justifyContent: 'flex-end'
-        },
-        modalViewR: {
-            paddingHorizontal: wp('4%'),
-            //  borderTopEndRadius: 3,
-            //  borderTopLeftRadius: 3,
-            // marginTop: Dimension.getHeight(18),
-            // height: Dimension.getHeight(50),
-            width: wp('100%'),
-            minHeight: hp('20%'),
-            maxHeight: hp('80%'),
-            //  width: Dimension.getWidth(95),
-            // alignItems: 'flex-start',
-            borderTopLeftRadius: wp('10%'),
-            borderTopRightRadius: wp('10%'),
-            borderColor: Colors.black,
-            borderWidth: wp('0.2%'),
-            //  alignSelf: 'center',
-            backgroundColor: '#5f9ea0',
-            backgroundColor: 'white',
-            shadowColor: '#000000',
-            shadowOffset: {
-                width: 2,
-                height: 2,
-            },
-            shadowOpacity: 1,
-            shadowRadius: 4,
-            elevation: 44,
-        },
-       bar: {
-            backgroundColor: Colors.dark_grey,
-            padding: wp('1%'),
-            width: wp('60%'),
-            alignSelf: 'center',
-            borderRadius: wp('100%'),
-            marginVertical: hp('0.5%')
-       },
-      InputView: {
-            borderRadius: 10,
-            marginHorizontal: wp('4%'),
-            borderWidth: wp('0.1%'),
-            borderRadius: wp('1%'),
-            padding: wp('2%'),
-
-        },
-        input: {
-            color: '#000000',
-            // width: wp('85%'),
-            justifyContent: 'center',
-            marginVertical: hp('1%'),
-            height: hp('6%'),
-            borderColor: Colors.dark_grey,
-            borderWidth: wp('0.1%'),
-            borderRadius: wp('1%'),
-            padding: wp('2%'),
-            // backgroundColor: Colors.white
-        },
-        rowlist: {
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: 'row',
-            borderBottomWidth: wp('0.1%'),
-            marginVertical: wp('1%'),
-            borderBlockColor: Colors.dark_grey
-        },
-        button: {
-            backgroundColor: Colors.Primary,
-            padding: wp('3%'),
-            width: "48%",
-            alignItems: 'center',
-            borderRadius: 10
-        },
-        txt_forgt: {
-            color: Colors.link_color
-        },
-        pic: {
-            width: wp('18%'),
-            height: hp('9%'),
-            borderWidth: wp('0.5%'),
-            borderColor: Colors.light_gray,
-            borderRadius: wp('100%'),
-            resizeMode: 'contain'
-            // marginRight:Dimension.getWidth(7)
-        },
-        box_Society:{  borderRadius: 10,
-            backgroundColor: '#ffffff',
-            height: hp('12%'),
-          //  width: wp('40%'),
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            paddingHorizontal: wp('2%'),
-            marginHorizontal:wp('0.5%'),
-            marginVertical:hp('1%'),
-            flexDirection:'row'},
-            txt_value: {
-                fontSize: wp('3.5%'),
-                color: Colors.black,
-                marginVertical: hp('0.5%'),
-                fontFamily:Fonts.Lato_Bold,
-            },
-            text_msg: {
-                fontSize: wp('4%'),
-                color: "black",
-                marginVertical: hp('1%'),
-                // fontFamily: Fonts.Relway_Bold,
-              },
-              button_book1: {
-                padding: wp('0.2%'),
-                width:wp('40%'),
-                backgroundColor: "#FFB61D",
-                borderRadius: wp('2%'),
-                marginVertical: hp('1%'),
-                alignItems: 'center',
-                alignSelf: 'flex-end',
-                // marginBottom:hp("4.5%")
-              
-              },
-              inputField: {
-                borderWidth: 1,
-                width: wp(75),
-                minHeight: 40,
-                borderRadius: 13,
-                color: "black",
-                borderColor: "lightgrey",
-                marginVertical: hp(1),
-                
-            },
+       
+      
+      
+     
+      
+     
+      
     }
 )
 export default styles;
